5ae542c599b47a84090c4c11cdb77973ec11587f
Justin R <jmr18c@acu.edu>
2022-04-27 07:09:55 -0500

    DONE

--- src/main/java/Toml/Example.java
+++ src/main/java/Toml/Example.java
@@ -4,6 +4,24 @@ import java.math.BigDecimal;
 import Toml.Toml;
 
 /**
+ * 	TOML (Tom's Obvious, Minimal Language) Java Implementation
+ *  Example.java
+ *
+ *	Authors: 	Brett Hammit (bah20a@acu.edu)
+ *			 	Alex Jackson (asj18a@acu.edu)
+ *			 	Justin Raitz (jmr18c@acu.edu)
+ *	Start Date:	03/08/22
+ *	Due Date:	04/27/22
+ *	Class:		CS375.01
+ *
+ *	Compile: 	Locate folder with maven pom.xml from command prompt
+ *				Run %mvn compile
+ *	Test:		%mvn test
+ *	Clean:		%mvn clean
+ *	Execute:	%java -cp target/classes/ Example
+ */
+
+/**
  * The Example class is the main class for the example implementation of Toml.java
  * @param
  */
@@ -16,15 +34,14 @@ public class Example {
         // Create Toml object
         Toml toml = new Toml();
         // Pass full path of filename to parseToml function
-        toml.parseToml(new File("/home/xanjaxn/Code/Java/old/SE2/src/main/resources/example.toml"));
+        toml.parseToml(new File("C:/Users/jrait/SE2/FightFight/SE2/src/main/resources/example.toml"));
 
         // Output using SE2Toml library's functionalities
         System.out.println("Hi, my name is " + toml.getString("name") + " and I am the database administrator.");
         System.out.println("I was born on " + toml.getOffsetDateTime("dob") + ".");
         System.out.println("Our database is located at: " + toml.getString("server") + ".");
         System.out.println("The connection port is: " + ((BigDecimal) toml.getArray("ports")[0]).intValue() + ".");
-        System.out.println("The connection port is: " + ((BigDecimal) toml.getArray("ports")[1]).intValue() + ".");
-        System.out.println("There you can access data like: " + (((Object[]) toml.getArray("data")[0])[0]).toString());
-        System.out.println("As well as data like: " + ((BigDecimal) ((Object[]) toml.getArray("data")[1])[0]).intValue());
+        System.out.println("There you can access data from arrays: " + (((Object[]) toml.getArray("data")[0])[0]).toString());
+        System.out.println("As well as data like integers: " + ((BigDecimal) ((Object[]) toml.getArray("data")[1])[0]).intValue());
     }
 }
